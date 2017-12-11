9b2eaa05c17b0815a8b15ae227011f929e200638
zhumenglimayer <657695558@qq.com>
2017-12-11 21:13:27 +0800

    todolist-react

---
+++ React/ToDoList/webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path')
+
+module.exports = {
+    entry: './src/entry.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.styl$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'stylus-loader'
+                ]
+            },
+            {
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            'es2015',
+                            'react'
+                        ]
+                    }
+                }
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif)$/,
+                use: {
+                    loader: 'url-loader',
+                    options: {
+                        limit: 8192
+                    }
+                }
+
+            }
+        ]
+    }
+}
